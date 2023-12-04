b1089ba0513181a4da88315eef35edc3c3e8726c
Dinis Cruz <dinis.cruz@owasp.org>
2023-12-04 01:26:51 +0000

    Improved tests for Build_Deploy__Docker_Linux_Python

--- osbot_lambdas/docker_linux_python/dev/Build_Deploy__Docker_Linux_Python.py
+++ osbot_lambdas/docker_linux_python/dev/Build_Deploy__Docker_Linux_Python.py
@@ -1,6 +1,9 @@
+import requests
 from osbot_aws.helpers.Create_Image_ECR import Create_Image_ECR
 from osbot_lambdas import docker_linux_python
+from osbot_utils.utils.Dev import pprint
 from osbot_utils.utils.Files import parent_folder
+from osbot_utils.utils.Misc import random_port
 
 
 class Build_Deploy__Docker_Linux_Python:
@@ -9,3 +12,31 @@ class Build_Deploy__Docker_Linux_Python:
         self.image_name       = 'docker_linux_python'
         self.path_images      = parent_folder(docker_linux_python.path)
         self.create_image_ecr = Create_Image_ECR(image_name=self.image_name, path_images=self.path_images)
+        self.api_docker       = self.create_image_ecr.api_docker
+
+    def build_docker_image(self):
+        self.create_image_ecr.ecr_login()               # needed to get the image from public.ecr.aws
+        return self.create_image_ecr.build_image()
+
+    def start_docker_container(self, local_port=9000):
+        port_bindings = {8080: local_port}
+        container     =  self.api_docker.container_create(image_name=self.repository(), port_bindings=port_bindings)
+        container.start()
+        return container
+
+    def execute_docker_container(self):
+        local_port = random_port()
+        container = self.start_docker_container(local_port=local_port)
+        url       = f"http://localhost:{local_port}/2015-03-31/functions/function/invocations"
+        response  = requests.post(url, json={})
+        container.stop()
+        container.delete()
+        return response.json()
+
+    def push_to_ecr(self):
+        if self.create_image_ecr.repository_exists() is False:      # make sure the repository exists in ecr
+            return self.create_image_ecr.create_repository()
+        return self.create_image_ecr.push_image()
+
+    def repository(self):
+        return self.create_image_ecr.image_repository()
--- tests/docker_linux_python/test_Build_Deploy__Docker_Linux_Python.py
+++ tests/docker_linux_python/test_Build_Deploy__Docker_Linux_Python.py
@@ -1,7 +1,10 @@
+import requests
 from unittest import TestCase
 
 from osbot_lambdas.docker_linux_python.dev.Build_Deploy__Docker_Linux_Python import Build_Deploy__Docker_Linux_Python
+from osbot_utils.utils.Dev import pprint
 from osbot_utils.utils.Files import folder_name
+from osbot_utils.utils.Misc import random_port
 
 
 class test_Build_Deploy__Docker_Linux_Python(TestCase):
@@ -13,3 +16,34 @@ class test_Build_Deploy__Docker_Linux_Python(TestCase):
         assert self.docker_linux_python.image_name                   == 'docker_linux_python'
         assert folder_name(self.docker_linux_python.path_images)     == 'osbot_lambdas'
         assert self.docker_linux_python.create_image_ecr.image_name  == 'docker_linux_python'
+
+    def test_a__build_docker_image(self):
+        result = self.docker_linux_python.build_docker_image()
+        assert result.get('status') == 'ok'
+
+    def test_b__start_docker_container(self):
+        local_port = random_port()
+        container  = self.docker_linux_python.start_docker_container(local_port=local_port)
+        url        = f"http://localhost:{local_port}/2015-03-31/functions/function/invocations"
+        response   = requests.post(url, json={})
+        status     = container.info().get('status')
+        container.stop()
+        container.delete()
+
+        assert status == 'running'
+        assert response.json() == {"statusCode": 200, "body": "Hello from Lambda!"}
+
+    def test_c__execute_docker_container(self):
+        response = self.docker_linux_python.execute_docker_container()
+        assert response == {"statusCode": 200, "body": "Hello from Lambda!"}
+
+    def test_d__push_to_ecr(self):
+        result          = self.docker_linux_python.push_to_ecr()
+        auth_result     = result.get('auth_result')
+        push_json_lines = result.get('push_json_lines')
+        assert auth_result.get('Status') == 'Login Succeeded'
+        assert 'errorDetail' not in push_json_lines
+
+    #def create_lambda
+
+
